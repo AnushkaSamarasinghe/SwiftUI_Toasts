6075efbf83767a19540d25dfec251bc431818199
Anushka <anushkapws@gmail.com>
2025-05-19 10:20:07 +0530

    Project setup

---
+++ SwiftUIToasts/SwiftUIToastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToastsApp.swift
+//  SwiftUIToasts
+//
+//  Created by Anushka Samarasinghe on 2025-05-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
